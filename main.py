419ba25dc44fcf6a8319236d29e74f37b08d932f
Antboy05 <antek.bialecki@gmail.com>
2026-03-20 14:22:53 +0100

    Commit z Spyder

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('IO_project')
+    print_hi('Spydera')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
