1bed891130f184cbc28cfa4c694dd39b2994cc37
Antboy05 <antek.bialecki@gmail.com>
2026-05-27 12:19:08 +0200

    Poprawa main.py
    dodanie "from sqlalchemy import text"i zmiana ip host-a

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 import database as db
+from sqlalchemy import text
 
 app = FastAPI()
 
@@ -17,7 +18,7 @@ def get_db():
 def test_db(session: Session = Depends(get_db)):
     try:
         # To zapytanie sprawdza tylko czy baza odpowiada
-        session.execute(db.text("SELECT 1"))
+        session.execute(text("SELECT 1"))
         return {"status": "Połączono pomyślnie!"}
     except Exception as e:
         return {"status": "Błąd", "details": str(e)}
@@ -45,4 +46,4 @@ def read_user(user_id: int, session: Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
